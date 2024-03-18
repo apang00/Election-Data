85f69805c5e204cb34c07b5b6da33086f8999bb7
Alex <alex.pang@jci.com>
2024-03-17 20:55:20 -0400

    completed model

--- Model.R
+++ Model.R
@@ -8,7 +8,14 @@
 
 #### Description ####
 # Since the data we will be looking at will be election results between two
-# Parties, I would be using a logistic regression. EXPAND
+# parties, I would be using a logistic regression to display my findings.
+# This is because the outcome is binary as there are two main candidate and 
+# will have the best results for interpretability.
+
+# Similar to Nate Cohn's New York Time's article, I will be looking at election
+# data from the United States. I will be focusing on election data from 2020 
+# with Biden and Trump as the binary outcomes and use the results from every
+# electoral state.
 
 library(boot)
 library(collapse)
@@ -22,3 +29,82 @@ library(rstanarm)
 library(tidybayes)
 library(tidyverse)
 library(dplyr)
+library(ggplot2)
+
+### Data Cleaning ###
+
+# Selects only the data for year of 2020 and filters out columns that
+# are not useful
+filtered_data <- X1976_2020_president %>% filter(year == 2020) %>%
+  select(year, state, candidate, candidatevotes, totalvotes)
+
+# adds percentage of ballots won
+filtered_data$win_perc <- round(filtered_data$candidatevotes / 
+                                  filtered_data$totalvotes * 100, digits = 0)
+
+filtered_data <- filtered_data %>%
+  filter(filtered_data$candidate == "BIDEN, JOSEPH R. JR" |
+           filtered_data$candidate == "TRUMP, DONALD J.")
+
+# replaces Bidens with 0 and Trumps with 1
+for (i in 1:nrow(filtered_data)) {
+  if (filtered_data$candidate[i] == "BIDEN, JOSEPH R. JR") {
+    filtered_data$candidate[i] <- 0
+  }
+  else if (filtered_data$candidate[i] == "TRUMP, DONALD J.") {
+    filtered_data$candidate[i] <- 1
+  }
+}
+
+# To replicate the New York Time's model, I will create a new column that shows
+# the win percent of one party over another.
+
+filtered_data$win_margin <- NA
+
+
+for (i in seq(1, nrow(filtered_data), by = 2)) {
+
+  value1 <- filtered_data$win_perc[i]
+  value2 <- filtered_data$win_perc[i + 1]
+  
+  win_margin <- max(value1, value2) - min(value1, value2)
+  
+  if (value1 > value2) {
+    filtered_data$win_margin[i] <- win_margin
+    filtered_data$win_margin[i + 1] <- NA
+  } else {
+    filtered_data$win_margin[i] <- NA
+    filtered_data$win_margin[i + 1] <- win_margin
+  }
+}
+
+winners <- subset(filtered_data, !is.na(win_margin))
+
+### Graphing ###
+
+bar_graph <- ggplot(winners, aes(x = factor(state), y = win_margin, fill = factor(candidate))) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = c("0" = "blue", "1" = "red")) + 
+  labs(x = "State", y = "Win Margin By %", title = "Win Margin Per State") +  
+  theme_minimal() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+  labs(fill = "Candidates")
+
+bar_graph
+
+
+### Analysis ###
+
+# From the bar graph, it seems like in most states, the voter percentage 
+# difference lies between 10-30% with some exceptions. For example The District
+# Of Columbia where Biden wins by over 80% margin and Wyoming where Trump wins 
+# by over 40% margin. There are also a few instances where there are ties such 
+# as Arizona and Georgia where the voters who voted for Trump and Biden are 
+# equal in percentage, thus not producing a margin. 
+
+# Compared to Nate Cohn's findings, the % differences in margin in my findings
+# were much greater. However, this makes sense because that data was gathered 
+# in polls held in smaller vicinities compared to ballots across whole states.
+
+
+
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,6 @@
 The election data was collected from Kaggle: https://www.kaggle.com/datasets/tunguz/us-elections-dataset
 
 To run Model, import the dataset into Model file and run.
+
+Further descriptions regarding choice of model, data cleaning, and result analysis can be found in
+the Model file.
